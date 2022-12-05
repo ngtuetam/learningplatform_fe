03cc439ab63b758625d13923796afca253b490d8
ngtuetam <tuetam011203@gmail.com>
2022-12-05 18:47:26 +0700

    refactor: convert courses list to a component

--- src/views/Courses.vue
+++ src/views/Courses.vue
@@ -30,31 +30,7 @@
                             v-for="course in courses"
                             v-bind:key="course.id"
                         >
-                            <div class="card">
-                                <div class="card-image">
-                                    <figure class="image is-4by3">
-                                        <!-- <img src="http://bulma.io/images/placeholders/1280x960.png" alt="placeholder image"> -->
-                                        <img src="https://websitehoctructuyen.com/wp-content/uploads/2020/08/e-learning-1024x636.jpg" alt="placeholder image">
-                                    </figure>
-                                </div>
-
-                                <div class="card-content">
-                                    <div class="media">
-                                        <div class="media-content">
-                                            <p class="is-size-5">{{ course.title }}</p>
-                                        </div>
-                                    </div>
-
-                                    <div class="content">
-                                        <p>{{ course.short_description}}</p>
-                                        <!-- <a href="#">More</a> -->
-                                        <!-- name=Course refer to Course router -->
-                                        <router-link :to="{name:'Course', params:{slug: course.slug} }">More</router-link>
-                                    </div>
-
-                                </div>
-
-                            </div>
+                            <CourseItem :course="course" />
                         </div>
 
                         <div class="column is-12">
@@ -85,12 +61,16 @@
 
 <script>
 import axios from 'axios'
+import CourseItem from '@/components/CourseItem.vue'
 
 export default {
     data() {
         return {
             courses: []
         }
+    },
+    components: {
+        CourseItem
     },
     mounted() {
         console.log('mounted')
